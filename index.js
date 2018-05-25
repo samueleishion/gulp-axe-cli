92a0ed9d14f130a48e2076f417369fdc78127396
Sam Acuna <samueleishion@gmail.com>
2018-05-25 16:14:28 -0500

    chore(lint): added standard for linting on precommit

--- index.js
+++ index.js
@@ -1,64 +1,62 @@
-'use strict';
+'use strict'
 
-const through = require('through2');
-const PluginError = require('plugin-error');
-const gutil = require('gulp-util');
-const exec = require('child_process').exec;
+const through = require('through2')
+const PluginError = require('plugin-error')
+const gutil = require('gulp-util')
+const exec = require('child_process').exec
 
-const PLUGIN_NAME = 'gulp-axe-cli';
+const PLUGIN_NAME = 'gulp-axe-cli'
 
 module.exports = options => {
-  options = options || {};
+  options = options || {}
 
   return through.obj(function (file, enc, cb) {
-    var self = this;
-
     if (file.isNull()) {
-      cb(null, file);
-      return;
+      cb(null, file)
+      return
     }
 
     if (file.isBuffer()) {
-      file.contents = Buffer.concat([file.contents]);
+      file.contents = Buffer.concat([file.contents])
     }
 
     if (file.isStream()) {
-      cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
-      return;
+      cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'))
+      return
     }
 
     try {
       (() => {
-        var buffer = file;
-        var contents = Buffer.from(buffer.contents.toString());
-        var params = options;
-        var url;
+        var buffer = file
+        // var contents = Buffer.from(buffer.contents.toString())
+        var params = options
+        var url
 
         if (typeof params.urls === 'function') {
-          url = params.urls(buffer.history[0]);
+          url = params.urls(buffer.history[0])
 
           if (url === undefined || url.length <= 0) {
-            this.emit('error', new PluginError(PLUGIN_NAME, 'the obj.url() must return a valid string'));
+            this.emit('error', new PluginError(PLUGIN_NAME, 'the obj.url() must return a valid string'))
           }
         } else {
-          url = buffer.history[0];
+          url = buffer.history[0]
         }
 
-        exec('axe "' + url + '" --tags wcag2a,wcag2aa', function(err, stdout, stderr) {
-          if (stdout.indexOf('Accessibility issues detected') > 0) {
-            gutil.log(gutil.colors.cyan(PLUGIN_NAME), gutil.colors.red("[ERROR]"), 'Error testing ' + url);
-            throw stdout;
-          } else if (stdout.indexOf('0 violations found!') > 0) {
-            gutil.log(gutil.colors.cyan(PLUGIN_NAME), gutil.colors.green("[PASS]"), url);
+        exec('axe "' + url + '" --tags wcag2a,wcag2aa', function (error, response, body) {
+          if (error !== null || response.indexOf('Accessibility issues detected') > 0) {
+            gutil.log(gutil.colors.cyan(PLUGIN_NAME), gutil.colors.red('[ERROR]'), 'Error testing ' + url)
+            throw response
+          } else if (error !== null || response.indexOf('0 violations found!') > 0) {
+            gutil.log(gutil.colors.cyan(PLUGIN_NAME), gutil.colors.green('[PASS]'), url)
           }
-        });
-      })();
+        })
+      })()
 
-      this.push(file);
+      this.push(file)
     } catch (err) {
-      this.emit('error', new PluginError(PLUGIN_NAME, err));
+      this.emit('error', new PluginError(PLUGIN_NAME, err))
     }
 
-    cb(null, file);
-  });
-};
+    cb(null, file)
+  })
+}
--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-import path from 'path';
-import test from 'ava';
-import Vinyl from 'vinyl';
-import pEvent from 'p-event';
-import fn from '.';
+import path from 'path'
+import test from 'ava'
+import Vinyl from 'vinyl'
+import pEvent from 'p-event'
+import fn from '.'
 
 test(async t => {
-	const stream = fn();
-	const promise = pEvent(stream, 'data');
+  const stream = fn()
+  const promise = pEvent(stream, 'data')
 
-	stream.end(new Vinyl({
-		base: __dirname,
-		path: path.join(__dirname, 'file.ext'),
-		contents: Buffer.from('unicorns')
-	}));
+  stream.end(new Vinyl({
+    base: __dirname,
+    path: path.join(__dirname, 'file.ext'),
+    contents: Buffer.from('unicorns')
+  }))
 
-	const file = await promise;
-	t.is(file.contents.toString(), 'unicorns');
-});
+  const file = await promise
+  t.is(file.contents.toString(), 'unicorns')
+})
